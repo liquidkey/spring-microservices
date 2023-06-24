724ef4d6106c3a985cb814e96e203a0326079037
Keyur <keyur.thadeshwar@gmail.com>
2023-06-24 17:24:13 +0100

    initial commit

---
+++ src/main/java/com/keyur/microshelloworld/MicrosHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.keyur.microshelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyur/microshelloworld/MicrosHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyur.microshelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
